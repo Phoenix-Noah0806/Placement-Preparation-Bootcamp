872a2f89428f084b17b8682fb43665c872490c2d
Phoenix-Noah0806 <aayushmaan.jaiswal0308@gmail.com>
2026-02-13 14:18:09 +0530

    Implemented session auth

--- Authentication and Authorization/session-auth.js
+++ Authentication and Authorization/session-auth.js
@@ -1,6 +1,6 @@
 // Session Authentication 
 import express from 'express'
-
+import session from 'express-session'
 const app=express()
 app.use(express.json())
 app.use(session({
@@ -13,6 +13,22 @@ const USER={
     username:"admin",
     password:"1234"
 }
+
+app.post('/login',(req ,res)=>{
+    const {username,password}=req.body
+    if(username===USER.username && password===USER.password){
+        req.session.user=username
+        return res.json({"Message": "Login Successful"})
+    }
+    res.status(401).json({"Message": "Invalid Credentials"})
+    })
+
+app.get('/dashboard',(req ,res)=>{
+    if(!req.session.user){
+        return res.status(401).json({"Message": "Unauthorized"})    
+    }
+    res.send(`Welcome to dashboard , ${req.session.user}`)
+})
 app.listen(3000,()=>{
     console.log("App is listening at http://localhost:3000")
 })
